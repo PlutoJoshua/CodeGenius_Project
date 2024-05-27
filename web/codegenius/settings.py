22ac1b7a937ade5f71b07153573813f05c687709
PiljuAn <pjahn7788@gmail.com>
2024-05-27 14:29:03 +0900

    ..

--- web/codegenius/settings.py
+++ web/codegenius/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'chatbot', 'static'),
-]
+# STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'chatbot', 'static'),
+#]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
