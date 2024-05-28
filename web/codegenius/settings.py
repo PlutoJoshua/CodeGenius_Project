24f6891fc135151f332cd5b411bc27dcc0f87834
sooooohyeon <gkrry963@gmail.com>
2024-05-28 18:00:57 +0900

    홈페이지 배경 이미지 추가 및 Start 버튼 위치 조정

--- web/codegenius/settings.py
+++ web/codegenius/settings.py
@@ -118,11 +118,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'chatbot', 'static'),
-#]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'chatbot', 'static'),
+]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
